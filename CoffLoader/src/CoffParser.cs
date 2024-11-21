6f34f8e204789f6691ba8efa1ff8cbda7e8804c0
coin <anthonyrose88@gmail.com>
2024-11-21 17:50:21 -0500

    fixed assembly name and removed runcoff.cs

--- CoffLoader/src/CoffParser.cs
+++ CoffLoader/src/CoffParser.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
-namespace COFFLoader
+namespace CoffLoader
 {
     unsafe class CoffParser
     {
--- CoffLoader/src/CoffStructs.cs
+++ CoffLoader/src/CoffStructs.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Runtime.InteropServices;
 
-namespace COFFLoader
+namespace CoffLoader
 {
     #region structs
     [StructLayout(LayoutKind.Sequential)]
